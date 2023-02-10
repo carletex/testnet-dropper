5d2dca92241db7f6026d44de1e2541e022a91da5
Carlos Sánchez <oceanrdn@gmail.com>
2023-02-10 13:43:59 +0100

    Faucet working

--- packages/nextjs/pages/api/trigger-faucet.tsx
+++ packages/nextjs/pages/api/trigger-faucet.tsx
@@ -1,9 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ethers } from "ethers";
 
+// in ETH
+const FAUCET_AMOUNT = "0.01";
+const infura_api_key = process.env.INFURA_API_KEY;
+const wallet_private_key = process.env.WALLET_PRIVATE_KEY;
+
 // ToDo. Protect endpoint with
 // ToDo. send funds
-export default function handler(request: NextApiRequest, response: NextApiResponse) {
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  if (!wallet_private_key) return;
   if (request.method !== "POST") {
     response.status(405).json({ message: "Only POST requests allowed" });
     return;
@@ -13,6 +19,29 @@ export default function handler(request: NextApiRequest, response: NextApiRespon
   if (!ethers.utils.isAddress(address)) {
     response.status(400).json({ message: "Wrong address" });
   }
+
+  // Init providers
+  const goerliProvider = new ethers.providers.InfuraProvider(5, infura_api_key);
+  const sepoliaProvider = new ethers.providers.InfuraProvider(11155111, infura_api_key);
+
+  // Init signers
+  const goerliWallet = new ethers.Wallet(wallet_private_key, goerliProvider);
+  const sepoliaWallet = new ethers.Wallet(wallet_private_key, sepoliaProvider);
+
+  // ToDo. handle errors.
+  const txGoerli = await goerliWallet.sendTransaction({
+    to: address,
+    value: ethers.utils.parseEther(FAUCET_AMOUNT),
+  });
+
+  console.log("GOERLI TX", txGoerli);
+
+  const txSepolia = await sepoliaWallet.sendTransaction({
+    to: address,
+    value: ethers.utils.parseEther(FAUCET_AMOUNT),
+  });
+
+  console.log("SEPOLIA TX", txSepolia);
 
   response.status(200).end();
 }
